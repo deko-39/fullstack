4ffd2d92616b9d03920932c2e36cd235c18ad799
Deko <tam.nguyen.cdt@gmail.com>
2024-01-23 22:16:34 +0700

    feat

--- day6/index.js
+++ day6/index.js
@@ -35,3 +35,25 @@ server.use("/index", (req, res) =>
 mongoose
   .connect("mongodb://localhost:27017/fullstack")
   .then(() => server.listen(3000, () => console.log("Server is running!")));
+
+// BTVN
+/**
+ * - User controller CRUD
+ * - User model {username, email, password}
+ * - Post controller CRUD
+ * - Post model {userId, content, date}
+ * - Comment controller CRUD
+ * - Comment model {postId, userId, content, date}
+ * - Middleware authentication cho toàn bộ app với header x-username and inject req.user = user from user collection
+ * - Middleware authorization check role "admin" from req.user để truy cập vào CRUD post và comment
+ * - Middleware validation:
+ *    + GET /posts/:postId thì yêu cầu req.user._id == post.userId (chỉ user tạo post mới xem được post)
+ *    + POST /posts thì yêu cầu đủ trường của model post
+ *    + PUT /posts/:postId thì yêu cầu user là người tạo post và đủ trường của model post
+ *    + DELETE /posts/:postId thì yêu cầu user là người tạo post
+ *
+ *    + GET /comments/:commentId thì yêu cầu req.user._id == comment.userId (chỉ user tạo comment mới xem được comment)
+ *    + POST /comments thì yêu cầu đủ trường của model comment
+ *    + PUT /comments/:commentId thì yêu cầu user là người tạo comment và đủ trường của model comment
+ *    + DELETE /comments/:commentId thì yêu cầu user là người tạo comment
+ */
