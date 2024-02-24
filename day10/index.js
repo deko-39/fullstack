e52d0863d98bc87598e50c770230f11782be4992
Deko <tam.nguyen.cdt@gmail.com>
2024-02-24 18:58:22 +0700

    feat

--- day10/index.js
+++ day10/index.js
@@ -39,6 +39,23 @@ const PostSchema = new Schema({
 
 const PostModel = mongoose.model("post", PostSchema);
 
+const UserSchema = new Schema({
+  name: String,
+  age: Number,
+  avatar: {
+    publicId: String,
+    url: String,
+  },
+  posts: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: PostSchema,
+    },
+  ],
+});
+
+const UserModel = mongoose.model("user", UserSchema);
+
 const memoryStorage = multer.memoryStorage();
 const upload = multer({ storage: memoryStorage });
 
@@ -106,10 +123,81 @@ server.delete("/posts/:postId", async (req, res) => {
 });
 
 // Register user + avatar
+server.post("/users", upload.single("avatar"), async (req, res) => {
+  try {
+    const files = req.files; // upload.array
+    if (files.length > 2 || files.every((file) => file.size > 5 * 1024)) {
+      res.status(400).send("Too many files or files too large");
+    }
+    const file = req.file; // upload.single
+    // const buffer = file.buffer?.data || []
+    const dataUrl = `data:${file.mimetype};base64,${file.buffer?.toString(
+      "base64"
+    )}`;
+    const fileName = file.originalname.split(".")[0];
+
+    const result = await cloudinary.uploader.upload(dataUrl, {
+      public_id: fileName,
+    });
+
+    const user = await UserModel.create({
+      name: req.body.name,
+      age: req.body.age,
+      avatar: { publicId: result.public_id, url: result.url },
+      posts: [],
+    });
+
+    res.status(201).send(user);
+  } catch (error) {
+    console.log("error :>> ", error);
+    res.status(500).send("Error!");
+  }
+});
+
 // Delete user
+server.post("/users/:userId", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userId);
+    if (!user) {
+      res.status(400).send("No user found!");
+    }
+
+    const publicId = user.avatar.publicId;
+    await cloudinary.uploader.destroy(publicId);
+    await UserModel.findByIdAndDelete(req.params.userId);
+    res.sendStatus(204);
+  } catch (error) {
+    console.log("error :>> ", error);
+    res.status(500).send("Error!");
+  }
+});
+
 // Update post
+server.put("/posts/:postId", upload.array("files"), async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("error :>> ", error);
+    res.status(500).send("Error!");
+  }
+});
+
 // GET post
+server.get("/posts", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("error :>> ", error);
+    res.status(500).send("Error!");
+  }
+});
+
 // User - post relationship
+server.get("/users", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log("error :>> ", error);
+    res.status(500).send("Error!");
+  }
+});
 
 mongoose
   .connect(process.env.MONGODB)
